9216fe7719d3f4e2f038b94d9dfe1a0238acbef1
Mohit Bhimrajka <mbhimrajka@supervity.ai>
2025-06-24 20:33:09 +0530

    Add broker connection retry on startup in Celery configuration to enhance reliability during service initialization.

--- celery_worker.py
+++ celery_worker.py
@@ -30,6 +30,7 @@ celery_app.conf.update(
     accept_content=['json'],
     timezone='UTC',
     enable_utc=True,
+    broker_connection_retry_on_startup=True,
 )
 
 # This is where we will tell Celery where to find our task function.
